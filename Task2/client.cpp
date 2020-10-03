e718141818c8418a584e3b6830764b4cf1a941f0
ercagarwal <sharan.n21@gmail.com>
2020-10-04 00:38:03 +0530

    changes to client

--- Task2/client.cpp
+++ Task2/client.cpp
@@ -15,6 +15,8 @@ using namespace std;
 
 char sender_buffer[1024];
 char receiver_buffer[1024];
+
+string sendDataStr, rcvDataStr;
 		
 void send_recv(int i, int sockfd)
 {
@@ -24,6 +26,7 @@ void send_recv(int i, int sockfd)
 	if (i == 0){
 
 		fgets(sender_buffer, BUFSIZE, stdin);
+		// getline(cin, sender_buffer);
 
 		if (strcmp(sender_buffer, "bye") == 0) {
 			exit(0);
@@ -34,35 +37,40 @@ void send_recv(int i, int sockfd)
 	else{
 		int message_size = recv(sockfd, receiver_buffer, BUFSIZE, 0);
 		receiver_buffer[message_size] = '\0';
+
 		cout << receiver_buffer << endl;
 		cout.flush();
 	}
 }
 		
-
 	
 int main()
 {
 	int sockfd, fdmax, i;
 	struct sockaddr_in server_addr;
-	fd_set master;
-	fd_set read_fds;
+	fd_set master, read_fds;
 
+
+	// SOCKET CREATION
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		cout << "failed to create socket" << endl;
 		exit(1);
 	}
 
+	// INIT SERVER ADDR
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(4950);
 	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	memset(server_addr.sin_zero, '\0', sizeof server_addr.sin_zero);
-	
+
+
+	// CONNECT TO SERVER
 	if(connect(sockfd, (struct sockaddr *) &server_addr, sizeof(struct sockaddr)) < 0) {
 		cout << "failed to connect to server" << endl;
 		exit(1);
 	}
 
+	// CONFIG THE FD BIT ARRAYS
 	FD_ZERO(&master);
 	FD_ZERO(&read_fds);
 	FD_SET(0, &master);
@@ -78,12 +86,13 @@ int main()
 			exit(1);
 		}
 		
-		for(i=0; i <= fdmax; i++ )
+		for(i=0; i <= fdmax; i++){
 			if(FD_ISSET(i, &read_fds))
 				send_recv(i, sockfd);
+		}
+			
 	}
 
 	close(sockfd);
-
 	return 0;
 }
